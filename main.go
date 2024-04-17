599d9714e4debe694c9d0668a10d8264c71fd979
Zadkiel Aharonian <zadkiel_aharonian@carrefour.com>
2024-04-17 08:55:46 +0000

    fix

--- main.go
+++ main.go
@@ -1,34 +1,23 @@
 package main
 
 import (
-        "crypto/rand"
+	"crypto/rand"
 	"encoding/base64"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"sort"
-	"strconv"
 	"time"
 
 	env "github.com/caarlos0/env/v8"
 )
 
-var (
-	
-        echoContext       bool
-	randomBytesCount  int
-	randomBytes       string
-	echoDelay         time.Duration
-	listenAddr        string
-)
-
 type config struct {
-	ShowContext       bool `env:"ECHO_CONTEXT" envDefault:"false"`
-	RandomBytes       int `env:"ECHO_BYTES" envDefault:"0"`
-	Delay             time.Duration `env:"ECHO_DELAY" envDefault:"0s"`
-	ListenAddress     string `env:"LISTEN_ADDRESS,required" envDefault:":8080"`
-	Debug             bool   `env:"DEBUG" envDefault:"false"`
+	ShowContext   bool          `env:"ECHO_CONTEXT" envDefault:"false"`
+	RandomBytes   int           `env:"ECHO_RAND_BYTES" envDefault:"0"`
+	Delay         time.Duration `env:"ECHO_DELAY" envDefault:"0s"`
+	ListenAddress string        `env:"ECHO_ADDR,required" envDefault:":8080"`
 }
 
 var (
@@ -37,13 +26,16 @@ var (
 	randomBytes string
 )
 
-func randomBytes(size int) (string, error) {
-   b := make([]byte, size)
-   _, err := rand.Read(b)
-   if err != nil {
-      return nil, err
-   }
-   return base64.StdEncoding.EncodeToString(b), nil
+func getRandomBytes(size int) string {
+	if size < 1 {
+		return ""
+	}
+	b := make([]byte, size)
+	_, err := rand.Read(b)
+	if err != nil {
+		return ""
+	}
+	return base64.StdEncoding.EncodeToString(b)
 }
 
 func logMiddleware(handler http.Handler) http.Handler {
@@ -56,13 +48,13 @@ func logMiddleware(handler http.Handler) http.Handler {
 func handleRequest(w http.ResponseWriter, r *http.Request) {
 	time.Sleep(time.Duration(cfg.Delay))
 	w.Header().Set("Content-Type", "text/plain")
-	if echoContext {
+	if cfg.ShowContext {
 		fmt.Fprintln(w, "X-Echo-Date:", time.Now().String())
-	        fmt.Fprintf(w, "X-Echo-Delay: %s\n", cfg.Delay)
+		fmt.Fprintf(w, "X-Echo-Delay: %s\n", cfg.Delay)
 		fmt.Fprintln(w, "X-Echo-Hostname:", hostname)
 		fmt.Fprintln(w, "X-Echo-Method:", r.Method)
 		fmt.Fprintln(w, "X-Echo-Proto:", r.Proto)
-	        fmt.Fprintf(w, "X-Echo-RandomBytes: %d\n", cfg.RandomBytes)
+		fmt.Fprintf(w, "X-Echo-RandomBytes: %d\n", cfg.RandomBytes)
 		fmt.Fprintln(w, "X-Echo-RemoteAddr:", r.RemoteAddr)
 		fmt.Fprintln(w, "X-Echo-URL:", r.URL)
 	}
@@ -94,14 +86,9 @@ func main() {
 
 	// cache values
 	hostname, _ = os.Hostname()
-	if cfg.RandomBytes > 0 {
-		randomString, err = randomBytes(cfg.RandomBytes)
-		if err != nil {
-			log.Fatalf("error: generate random bytes: %+v\n", err)
-		}
-	}
+	randomBytes = getRandomBytes(cfg.RandomBytes)
 
-	log.Printf("Listening on %s\n", cfg.ListenAddr)
+	log.Printf("Listening on %s\n", cfg.ListenAddress)
 	http.HandleFunc("/-/ready", handleReadinessRequest)
 	http.HandleFunc("/", handleRequest)
 	http.ListenAndServe(cfg.ListenAddress, logMiddleware(http.DefaultServeMux))
